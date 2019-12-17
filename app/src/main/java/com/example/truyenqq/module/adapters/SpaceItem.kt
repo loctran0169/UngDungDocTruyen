7ccb57cc510c1443b11ed88d47a372894aa91377
Trần Hữu Lộc <loctran0169@gmail.com>
2019-12-17 19:36:41 +0700

    base api

---
+++ app/src/main/java/com/example/truyenqq/module/adapters/SpaceItem.kt
@@ -0,0 +1,14 @@
+package com.example.truyenqq.module.adapters
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SpaceItem(private val space: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        outRect.left = space
+        outRect.right = space
+        outRect.bottom = space
+        outRect.top = space
+    }
+}
